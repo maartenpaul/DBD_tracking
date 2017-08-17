f558ee6012c1af05b8954e732ad57d1e3ddc45a3
maartenpaul <maarten.paul@gmail.com>
2017-08-17 12:14:06 +0200

    Update MSD analysis

--- R/MSD.R
+++ R/MSD.R
@@ -1,49 +1,54 @@
-track_msd <- function(tracks,n=5,framerate=30,pxsize=100){
-  tracks[,2] <- (tracks[,2]*pxsize)/1000
-  tracks[,3] <- (tracks[,3]*pxsize)/1000
+track_msd <- function(x,n=5,framerate=30,pxsize=100){
+  x$X <- (x$X*pxsize)/1000
+  x$Y <- (x$Y*pxsize)/1000
 
-  coef <- ddply(tracks,.variables = "track",.fun= function(x) {
+  coef <- ddply(x,.variables = "track",.fun= function(x) {
     result <- vector(length = n)
     #put first frame of track at zero
-    x[,1]  <- x[,1]-x[1,1]+1
+    x$frame  <- x$frame-x$frame[1]+1
     if(nrow(x)>n+1){
-      for (j in 1:n){
-        sum <- 0
-        n_sum <- 0
-        for(k in 1:(nrow(x))){
+      sapply(1:n,function(j){
+
+
+        sum <- unlist(sapply(1:nrow(x),simplify = F,function(k){
           if (is.element(x[k,1]+j,x[,1])){
             which_point <- which(x[,1]==x[k,1]+j)
-            sum <- sum+((x[k,2]-x[which_point,2])^2+(x[k,3]-x[which_point,3])^2)
-            n_sum <- n_sum+1
-          }
-        }
-        result[j] <- sum/n_sum
+            sum <- ((x[k,2]-x[which_point,2])^2+(x[k,3]-x[which_point,3])^2)
+            return(sum)
 
-      }
+        }}))
+
+        result[j] <<- mean(sum)
+
+    })
       return(result)
-    }
 
+    }
   })
   return(coef)
 }
 
 
-TRACK_MSD <- function(tracks,n=5,framerate=30,pxsize=100){
+TRACK_MSD <- function(x,n=5,framerate=30,pxsize=100){
   UseMethod("TRACK_MSD")
 }
 
-TRACK_MSD.default <- function(tracks,n=5,framerate=30,pxsize=100){
+TRACK_MSD.default <- function(x,n=5,framerate=30,pxsize=100){
   stop("MSD requires data frame")
 }
 
-TRACK_MSD.data.frame <-  function(tracks,n=5,framerate=30,pxsize=100){
-  track_msd(tracks,n,framerate,pxsize)
+TRACK_MSD.data.frame <-  function(x,n=5,framerate=30,pxsize=100){
+  track_msd(x,n,framerate,pxsize)
 
 }
 
-TRACK_MSD.list <-  function(tracks,n=5,framerate=30,pxsize=100){
+TRACK_MSD.list <-  function(x,n=5,framerate=30,pxsize=100){
+
   llply(x,function(x){
-    TRACK_MSD(tracks,n,framerate,pxsize)
+    out <- TRACK_MSD(x,n,framerate,pxsize)
+    out$cellID <- x$cellID
+    return(out)
   })
+
 }
 
--- R/MSD_fit.R
+++ R/MSD_fit.R
@@ -1,46 +1,36 @@
 
 
-track_msd_fit <- function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=0.01){
+track_msd_fit <- function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=4*0.01^2){
 
   #apply function for every track (column V4)
-  coef <- adply(x,.margins=1, .expand=FALSE,function(x) {
+  coef <- ddply(x,.variables = "track", function(x) {
     mst <- t(x[,-1])
     time <- seq(1,length(mst),1)
     time <- time/framerate/1000 #covert frame to second
     #plot(mst,ylim=c(0,0.5),xlim=c(0,10))
 
-    if(!fitzero){
+    if(fitzero){
+
       mst <- mst-offset
+
+      fit <- tryCatch(lm(formula = mst ~ time-1), error=function(e) NULL)
+
+    } else if (fitzero==FALSE) {
+
+      fit <- tryCatch(nls(formula = mst[,1] ~ time*x1+x2,lower = list(x1=0,x2=0),start=list(x1=0,x2=0),algorithm = "port"), error=function(e) NULL)
     }
-    #fit <- lm(mst[,1] ~ time+0)
-    # fit <- tryCatch(nls(formula = mst ~ time*x1+0,lower = list(x1=0,x2=-1),start=list(x1=0,x2=0),algorithm = "port"), error=function(e) NULL)
-    fit <- tryCatch(lm(formula = mst ~ time-1), error=function(e) NULL)
     if (!is.null(fit)){
       RSS.p <- sum(residuals(fit)^2)
       TSS <- sum((mst - mean(mst,na.rm = T))^2,na.rm = T)
       rsq <- 1-(RSS.p/TSS)
 
-      coef <- c(coef(fit),0.02,rsq)
+      coef <- c(coef(fit)[1],offset,rsq)
+      coef[1] <- coef[1]/4
+      return(coef)
+
     }
 
-    #           else if (fitzero==FALSE) {
-    #       #fit <- lm(mst[,1] ~ time)
-    #       fit <- tryCatch(nls(formula = mst[,1] ~ time*x1+x2,lower = list(x1=0,x2=0),upper = list(x1=100,x2=0.05), start=list(x1=0,x2=0),algorithm = "port"), error=function(e) NULL)
-    #
-    #       if (!is.null(fit)){
-    #         RSS.p <- sum(residuals(fit)^2)
-    #         TSS <- sum((mst[,1] - mean(mst[,1]))^2)
-    #         rsq <- 1-(RSS.p/TSS)
-    #           coef <- c(coef(fit),rsq)
-    #       } else {
-    #         coef <- c(NA,NA,NA)
-    #       }
-    #
-    #     }
     # MSD=4*D*t
-    coef[1] <- coef[1]/4
-    #coef[1] <- as.numeric(coef[1]/4)
-    return(coef)
 
 
   })
@@ -49,21 +39,21 @@ track_msd_fit <- function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=0.01)
   return (coef)
 }
 
-TRACK_MSD_fit <- function(x,y,prec,ch, px,xlim,ylim,file,output,fit,fast){
+TRACK_MSD_fit <- function(x,n,fitzero,framerate,pxsize,offset){
   UseMethod("TRACK_MSD_fit")
 }
 
-TRACK_MSD_fit.default <- function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=0.01){
+TRACK_MSD_fit.default <- function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=4*0.01^2){
   stop("MSD_fit requires data frame")
 }
 
-TRACK_MSD_fit.data.frame <-  function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=0.01){
+TRACK_MSD_fit.data.frame <-  function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=4*0.01^2){
   track_msd_fit(x,n,fitzero,framerate,pxsize,offset)
 
 }
 
-TRACK_MSD_fit.list <-  function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=0.01){
-  llply(x,function(x){
+TRACK_MSD_fit.list <-  function(x,n=5,fitzero=T,framerate=1/33,pxsize=100,offset=4*0.01^2){
+  llply(x,.progress = "text",function(x){
     TRACK_MSD_fit(x,n,fitzero,framerate,pxsize,offset)
   })
 }
