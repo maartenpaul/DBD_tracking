0b783be738e80668c6d54b8fa732a4c0fb03c919
maartenpaul <maarten.paul@gmail.com>
2019-04-04 17:16:28 +0200

    Added Z voxel size

--- scripts/Batch_to_spoton.R
+++ scripts/Batch_to_spoton.R
@@ -1,5 +1,5 @@
 directory <- "D:/Stack/Genetics/171024 PALB2-Halo tracking/"
-directory <- "D:/Stack/Genetics/170809 tracking MMC WT_dDBD_+CTD/"
+directory <- "F:/181012 BRCA2-Halo PCNA-iRFP IR tracking"
 
 framerate <- 1/32 #1/200 #ms
 pixelsize <- 100 #nm
